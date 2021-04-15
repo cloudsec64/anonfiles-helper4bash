5e1b6c26447fcf8131bb39e3698219ce5bd81ee0
cloudsec64 <82545423+cloudsec64@users.noreply.github.com>
2021-04-15 23:44:11 +0200

    Le main screept

---
+++ anonhelper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function anonhelp() {
+  echo "upload:upload a file to anonfiles syntax: upload filename.txt"
+  echo "info: get info about a file uploaded on anonfiles syntax: info fileid"
+}
+
+function anonupload() {
+  curl -F "file=@$1" https://api.anonfiles.com/upload
+}
+
+
+function anonupload() {
+  curl https://api.anonfiles.com/v2/file/$1/info
+}
